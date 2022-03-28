64177cf8474a9c69a50dd85c3392ce2ca8a48177
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-03-28 10:07:59 -0700

    Added Function and File Structure

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+function navigatePage(location) {
+    switch(location) {
+        case 'mainPage':
+            alert("Main Page")
+            break;
+        case 'pedigreePage':
+            alert("Pedigree Page")
+            break;
+        case 'endPage':
+            alert("Ending Page")
+            break;
+        default:
+            alert("Not Found")
+
+    }
+}
